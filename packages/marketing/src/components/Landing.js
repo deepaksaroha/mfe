27ccaf5450f11812adec62397b05288ec92d0019
deepaksaroha <deepaksaroha1993@gmail.com>
2025-05-16 15:59:08 +0530

    fix

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -14,7 +14,7 @@ import { Link } from "react-router-dom";
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright "}
+            {"Copyright © "}
             <MaterialLink component={Link} to="/" color="inherit">
                 Your Website
             </MaterialLink>
