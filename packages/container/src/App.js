3ad910696d6b359149b3c69eb69be2e460e78d28
deepaksaroha <deepaksaroha1993@gmail.com>
2025-05-18 01:02:23 +0530

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,18 @@
-import React, { Suspense, lazy, useState } from "react";
+import React, { Suspense, lazy, useState, useEffect } from "react";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {
     StylesProvider,
     createGenerateClassName,
 } from "@material-ui/core/styles";
 import Progress from "./components/Progress";
+import { createBrowserHistory } from 'history';
 
 const MarketingLazy = lazy(() => import("./components/MarketingApp"));
 const AuthLazy = lazy(() => import("./components/AuthApp"));
+const DashboardLazy = lazy(() => import("./components/DashboardApp"));
+
+const history = createBrowserHistory();
 
 const generateClassName = createGenerateClassName({
     productionPrefix: "co",
@@ -17,8 +21,14 @@ const generateClassName = createGenerateClassName({
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
 
+    useEffect(() => {
+        if(isSignedIn){
+            history.push('/dashboard');
+        }
+    }, [isSignedIn])
+
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header
@@ -32,11 +42,15 @@ export default () => {
                                     onSignIn={() => setIsSignedIn(true)}
                                 />
                             </Route>
+                            <Route path="/dashboard">
+                                {!isSignedIn && <Redirect to="/" />}
+                                <DashboardLazy />
+                            </Route>
                             <Route path="/" component={MarketingLazy} />
                         </Switch>
                     </Suspense>
                 </div>
             </StylesProvider>
-        </BrowserRouter>
+        </ Router>
     );
 };
---
+++ packages/dashboard/src/components/Dashboard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+        <button @click="changeMessage">Change Message</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "Hello, Dashboard!",
+            };
+        },
+        methods: {
+            changeMessage() {
+                this.message = "Message Changed!";
+            },
+        },
+    };
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
